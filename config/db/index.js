2c107e65c8f6a0aea172bd5ee8ac58365f96df6a
Phạm Ngọc Quốc Khánh <phamngocquockhanh2004@gmail.com>
2024-06-08 11:28:26 +0700

    chage permisson and port

--- config/db/index.js
+++ config/db/index.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 async function connect(){
     try {
-        await mongoose.connect('mongodb+srv://nguyenpham0666:7eW8rlCQvod1YTRS@pma101.efiuq1h.mongodb.net/PMA101');
+        await mongoose.connect('mongodb://103.72.96.121:29204/PMA101');
         console.log("ket noi thanh cong ")
     } catch (error) {
         console.log("ket noi thất bại ")
